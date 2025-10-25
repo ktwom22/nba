7ea18c4146251254a8dfebf9acecebe6beb6dfe9
Kyle Twombley <ktwom22@gmail.com>
2025-10-25 13:37:09 -0400

    fix a bunch of files

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from lineup_optimizer import load_players, generate_top_k
 app = Flask(__name__)
 
 # Example Google Sheet CSV URL
-SHEET_URL = "YOUR_GOOGLE_SHEET_CSV_URL"
+SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTF0d2pT0myrD7vjzsB2IrEzMa3o1lylX5_GYyas_5UISsgOud7WffGDxSVq6tJhS45UaxFOX_FolyT/pub?gid=324730904&single=true&output=csv"
 
 @app.route("/")
 def index():
