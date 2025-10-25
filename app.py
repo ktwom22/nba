ca82d6b1155d43776f9ba531004dc0e586f4ba5c
Kyle Twombley <ktwom22@gmail.com>
2025-10-25 15:17:27 -0400

    fix a bunch of files

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from lineup_optimizer import load_players, generate_top_k
 
 app = Flask(__name__)
 
-# Replace with your Google Sheet CSV URL (or local CSV path for testing)
 SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTF0d2pT0myrD7vjzsB2IrEzMa3o1lylX5_GYyas_5UISsgOud7WffGDxSVq6tJhS45UaxFOX_FolyT/pub?gid=324730904&single=true&output=csv"
 
 @app.route("/")
@@ -17,7 +16,7 @@ def index():
 @app.route("/optimize", methods=["POST"])
 def optimize():
     try:
-        data = request.json
+        data = request.get_json()
         players = data.get("players", [])
         count = int(data.get("player_count", 10))
         results = generate_top_k(players, count)
